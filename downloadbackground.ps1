200ca74133201330cb62297c53d972a8cd60e15a
admings25 <sysadm@gs25.com.vn>
2023-06-21 12:49:26 +0700

    test

---
+++ downloadbackground.ps1
@@ -0,0 +1,10 @@
+﻿$BackgroundImageURL = 'https://wiki.gs25.com.vn/background.png'
+$LockscreenImageURL = 'https://wiki.gs25.com.vn/locksreen.jpg'
+$ImageDestinationFolder = "C:\intune"
+$Backgroundimage = "$ImageDestinationFolder\background.png"
+$LockScreenImage = "$ImageDestinationFolder\locksreen.jpg"
+#Create Temp Image Directory
+md $ImageDestinationFolder -erroraction silentlycontinue
+#download images
+Start-BitsTransfer -Source $BackgroundImageURL -Destination "$Backgroundimage"
+Start-BitsTransfer -Source $LockscreenImageURL -Destination "$LockScreenimage"
